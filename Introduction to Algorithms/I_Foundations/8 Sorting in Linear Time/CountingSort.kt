f2f87c611c1f30aac9a8e63a542a96a334252a0c
Ambrosio-Jose <jose.ambrosio@ace.fan>
2022-08-02 17:32:53 -0700

    adds counting sort

---
+++ Introduction to Algorithms/I_Foundations/8 Sorting in Linear Time/CountingSort.kt
@@ -0,0 +1,38 @@
+package I_Foundations.`8 Sorting in Linear Time`
+
+
+/**
+ * Counting sort
+ *
+ * Assumes that each of the n input elements is between 0 and k.
+ * When k = O(n) the sort time is θ(n)
+ *
+ * @param list where each element at i is 0 <= list[i] <= [k]
+ * @param k
+ * @return sorted list
+ */
+fun countingSort(list: MutableList<Int>, k : Int): List<Int>{
+    val auxList = MutableList(k + 1) {0}
+    val results = MutableList(list.size){0}
+    list.forEach {num ->
+        auxList[num]++
+    }
+    // At this point the auxList contains the count of each element
+
+    for(i in 1 .. k){
+       auxList[i] += auxList[i - 1]
+    }
+
+    // At this point the auxList at each element contains the count of numbers equal to or less than the current number
+//    println("input $list")
+//    println("aux ${auxList}")
+    for(i in list.size - 1 downTo 0){
+//        print("i $i : list[i] ${list[i]} aux[i] ${auxList[list[i]]} ")
+        results[auxList[list[i]] - 1] = list[i]
+//        println(" results ${results.toTypedArray().contentDeepToString()}")
+        // This is decremented in case there are multiple elements with the same value. The next element with the same
+        // value will be inserted at the position before the previous element.
+        auxList[list[i]]--
+    }
+    return results
+}
---
+++ Introduction to Algorithms/I_Foundations/Test/CountingSortTest.kt
@@ -0,0 +1,97 @@
+package I_Foundations.Test
+
+import I_Foundations.`8 Sorting in Linear Time`.countingSort
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+internal class CountingSortTest {
+
+    @Test
+    fun test1() {
+        val sortingList = mutableListOf(5,2,4,6,1,3)
+        val sortedList = mutableListOf(1,2,3,4,5,6)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 6))
+    }
+
+    @Test
+    fun test2() {
+        val sortingList = mutableListOf(5,2,4,6,1,3,7)
+        val sortedList = mutableListOf(1,2,3,4,5,6,7)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 7))
+    }
+
+    @Test
+    fun test3() {
+        val sortingList = mutableListOf(5,2,6,4,6,1,3,9,1)
+        val sortedList = mutableListOf(1,1,2,3,4,5,6,6,9)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 9))
+    }
+
+    @Test
+    fun test4() {
+        val sortingList = mutableListOf(5,2,4,6,1,3,3,1000)
+        val sortedList = mutableListOf(1,2,3,3,4,5,6,1000)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 1000))
+    }
+
+    @Test
+    fun orderedList() {
+        val sortingList = mutableListOf(1,2,3,4,5,6,7)
+        val sortedList = mutableListOf(1,2,3,4,5,6,7)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 7))
+    }
+
+    @Test
+    fun reversedList() {
+        val sortingList = mutableListOf(7,6,5,4,3,2,1)
+        val sortedList = mutableListOf(1,2,3,4,5,6,7)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 7))
+    }
+
+    @Test
+    fun singleValue() {
+        val sortingList = mutableListOf(1)
+        val sortedList = mutableListOf(1)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 1))
+    }
+
+    @Test
+    fun twoValues() {
+        val sortingList = mutableListOf(3,2)
+        val sortedList = mutableListOf(2,3)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 3))
+    }
+
+    @Test
+    fun emptyList(){
+        val sortingList = mutableListOf<Int>()
+        val sortedList = mutableListOf<Int>()
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 0))
+    }
+
+    @Test
+    fun zeroList(){
+        val sortingList = mutableListOf<Int>(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        val sortedList = mutableListOf<Int>(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 0))
+    }
+
+    @Test
+    fun sameValueList(){
+        val sortingList = mutableListOf<Int>(5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5)
+        val sortedList = mutableListOf<Int>(5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5)
+
+        Assertions.assertEquals(sortedList, countingSort(sortingList, 5))
+    }
+}
