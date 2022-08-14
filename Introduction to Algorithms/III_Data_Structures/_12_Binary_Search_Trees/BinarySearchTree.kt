51e731b16a07f388ed0c27d207eb03a969e0745f
Ambrosio-Jose <jose.ambrosio@ace.fan>
2022-08-13 20:04:44 -0700

    set up for bst

---
+++ Introduction to Algorithms/III_Data_Structures/_12_Binary_Search_Trees/BinarySearchTree.kt
@@ -0,0 +1,16 @@
+package III_Data_Structures._12_Binary_Search_Trees
+
+class BinarySearchTree {
+
+    fun insert(value: Int){
+
+    }
+
+    fun search(value: Int): Int{
+       return 0
+    }
+
+    fun delete(value: Int) {
+
+    }
+}
